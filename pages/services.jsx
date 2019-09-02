6b5219cc1d6b5bc2523bc6fec2e1f7b3975f5333
ashway <kostya.kzo@gmail.com>
2019-09-02 14:14:29 +0500

    services list page refactor list request

--- pages/services.jsx
+++ pages/services.jsx
@@ -31,20 +31,18 @@ class ModelsPage extends React.Component {
     static async getInitialProps (ctx) {
         const { authToken } = cookies(ctx);
         let axios = axiosInstance(authToken);
-        let mark  = await axios.get(`${apiHost}/mark/list`);
-        let model  = await axios.get(`${apiHost}/model/list`);
         let services  = await axios.get(`${apiHost}/services/list`);
         let servicesList = services.data;
-
         _.each(servicesList, s=>s.models = _.compact((s.models||'').split(',')));
-        return { authToken, markList: _.keyBy(mark.data, 'alias'), servicesList: _.keyBy(servicesList, 'alias'), modelList: _.sortBy(_.remove(model.data, i=>!i.is_group), 'mark') };
+        let models = await axios.get(`${apiHost}/public/model/list`);
+        return { authToken, servicesList: _.keyBy(servicesList, 'alias'), modelList: _.orderBy(_.reject(models.data, { is_group: 1 }), 'markName') };
     }
 
     componentDidMount() {
         this.setState({ servicesList: this.props.servicesList });
     }
 
-    setService = (alias)=> {
+    setService = async (alias)=> {
         this.setState({ currentService: (this.state.currentService==alias)?null:alias });
     };
 
@@ -79,8 +77,8 @@ class ModelsPage extends React.Component {
                                 <div className="flex-block fb-vcenter"><div className="h1">{_.keyBy(serviceList, 'alias')[this.state.currentService].name}</div></div>
                                 <div className="isActive">
                                     {_.map(this.props.modelList, i=><div className={(_.includes((this.state.servicesList[this.state.currentService] || {}).models, i.alias))?'blue':''} key={i.alias} onClick={()=>this.setServiceCar(i.alias)}>
-                                        <div>{(this.props.markList[i.mark]||{}).name}</div>
-                                        <div className="bold">{i.name}</div>
+                                        <div>{i.markName}</div>
+                                        <div className="bold">{i.modelName}</div>
                                     </div>)}
                                 </div>
                             </div>
